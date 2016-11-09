40f71a5e3264f55dc13a59963b1aa09f7ab00bd1
nathantspencer <nathantspencer@gmail.com>
2016-11-09 16:47:48 -0500

    hoc commenter works dandily

--- swc_tools/swc2hoc/hoc_commenter.py
+++ swc_tools/swc2hoc/hoc_commenter.py
@@ -1,9 +1,13 @@
-import sys
 import code
+import sys
+import re
 
 def comment(swc_path):
 	f = open(swc_path, 'r')
 	swc_lines = f.readlines()
+	section_kidindex = { 1 : 1 }
+	section_prefix = { 1 : 'd1,' }
+	f.close()
 
 	# add comment line to indicate soma
 	soma_comment_index = -1
@@ -21,9 +25,34 @@ def comment(swc_path):
 			break
 	swc_lines.insert(branch1_comment_index, '\n// d1')
 
+	# set some key variables, create working copy of hoc text
+	comment_offset = 2
+	pattern = "connect sections\[([0-9]+)\]\(0\), sections\[([0-9]+)\]\(1\)"
+	swc_lines_output = []
+	for line in swc_lines:
+		swc_lines_output.append(line)
+
+	# add comments to the remaining branches
+	for ii, line in enumerate(swc_lines):
+		if(ii > branch1_comment_index + 4):
+			results = re.search(pattern, line)
+			if results:
+
+				# write comment line for branch
+				current = int(results.group(1))
+				parent = int(results.group(2))
+				comment = "// " + section_prefix[parent] + str(section_kidindex[parent]) + "\n"
+				swc_lines_output.insert(ii - comment_offset, comment)
+
+				# update dictionaries and offset
+				section_prefix[current] = comment[3:-1] + ','
+				section_kidindex[parent] = section_kidindex[parent] + 1
+				section_kidindex[current] = 1
+				comment_offset = comment_offset - 1
+
 	# save the updated hoc with the same path but altered name
 	f = open(swc_path[:-4] + "_commented.hoc", 'w')
-	for line in swc_lines:
+	for line in swc_lines_output:
 		f.write(line)
 	f.close()
 
