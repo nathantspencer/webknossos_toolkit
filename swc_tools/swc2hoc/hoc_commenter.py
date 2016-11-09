f5aac4f8995bd6c2e5b31c23ab48e32d8fe88912
nathantspencer <nathantspencer@gmail.com>
2016-11-09 17:12:34 -0500

    change many occurances of swc to hoc

--- swc_tools/swc2hoc/hoc_commenter.py
+++ swc_tools/swc2hoc/hoc_commenter.py
@@ -2,38 +2,40 @@ import code
 import sys
 import re
 
-def comment(swc_path):
-	f = open(swc_path, 'r')
-	swc_lines = f.readlines()
+def comment(hoc_path):
+
+	# read lines from hoc file, create dictionaries
+	f = open(hoc_path, 'r')
+	hoc_lines = f.readlines()
 	section_kidindex = { 1 : 1 }
 	section_prefix = { 1 : 'd1,' }
 	f.close()
 
 	# add comment line to indicate soma
 	soma_comment_index = -1
-	for ii, line in enumerate(swc_lines):
+	for ii, line in enumerate(hoc_lines):
 		if line == "access sections[0]\n":
 			soma_comment_index = ii - 1
 			break
-	swc_lines.insert(soma_comment_index, '// soma\n')
+	hoc_lines.insert(soma_comment_index, '// soma\n')
 
 	# add comment line to indicate first branch
 	branch1_comment_index = -1
-	for ii, line in enumerate(swc_lines):
+	for ii, line in enumerate(hoc_lines):
 		if line == "access sections[1]\n":
 			branch1_comment_index = ii - 1
 			break
-	swc_lines.insert(branch1_comment_index, '\n// d1')
+	hoc_lines.insert(branch1_comment_index, '\n// d1')
 
 	# set some key variables, create working copy of hoc text
 	comment_offset = 2
 	pattern = "connect sections\[([0-9]+)\]\(0\), sections\[([0-9]+)\]\(1\)"
-	swc_lines_output = []
-	for line in swc_lines:
-		swc_lines_output.append(line)
+	hoc_lines_output = []
+	for line in hoc_lines:
+		hoc_lines_output.append(line)
 
 	# add comments to the remaining branches
-	for ii, line in enumerate(swc_lines):
+	for ii, line in enumerate(hoc_lines):
 		if(ii > branch1_comment_index + 4):
 			results = re.search(pattern, line)
 			if results:
@@ -42,7 +44,7 @@ def comment(swc_path):
 				current = int(results.group(1))
 				parent = int(results.group(2))
 				comment = "// " + section_prefix[parent] + str(section_kidindex[parent]) + "\n"
-				swc_lines_output.insert(ii - comment_offset, comment)
+				hoc_lines_output.insert(ii - comment_offset, comment)
 
 				# update dictionaries and offset
 				section_prefix[current] = comment[3:-1] + ','
@@ -51,14 +53,14 @@ def comment(swc_path):
 				comment_offset = comment_offset - 1
 
 	# save the updated hoc with the same path but altered name
-	f = open(swc_path[:-4] + "_commented.hoc", 'w')
-	for line in swc_lines_output:
+	f = open(hoc_path[:-4] + "_commented.hoc", 'w')
+	for line in hoc_lines_output:
 		f.write(line)
 	f.close()
 
 if __name__ == "__main__":
 	if len(sys.argv) != 2:
 		print('\nhoc_commenter -- Written by Nathan Spencer 2016')
-		print('Usage: python hoc_commenter.py "path/to/swc/file.swc"')
+		print('Usage: python hoc_commenter.py "path/to/hoc/file.hoc"')
 	else:
 		comment(sys.argv[1])
